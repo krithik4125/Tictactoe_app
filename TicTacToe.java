9e7028c878f46e91b72c20c39c140e55277ad648
R KRITHIK SURIYA <irisfn4125@gmail.com>
2026-04-21 14:34:20 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class TicTacToe{
+ public static void printBoard() {
+        System.out.println("Board:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+            if (i < 2) System.out.println("---+---+---");
+        }
+    }

9ba9d517f67886a65e537d7459ea58f422ae807c
R KRITHIK SURIYA <irisfn4125@gmail.com>
2026-04-21 14:40:15 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TicTacToe {
+
+    public static void main() {
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
+    }
+}
